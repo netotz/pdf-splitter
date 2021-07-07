f41d022130fd30a4dc459f568ef3f55ee29e1f58
netotz <neto.otz@hotmail.com>
2021-07-07 10:55:31 -0500

    Add InputFile class

--- pdf-splitter/editor.py
+++ pdf-splitter/editor.py
@@ -1,21 +1,30 @@
 from dataclasses import dataclass
 from typing import List, Tuple
+from pathlib import Path
+import re
 
 from PyPDF2 import PdfFileReader, PdfFileWriter
 
+class InputFile:
+    def __init__(self, rawpath: str) -> None:
+        self.path = Path(rawpath)
+        self.abspath = self.path.as_posix()
+        self.name = self.path.name
+        self.id = re.match(r'^\d+', self.name).group()
+
 @dataclass
 class SplitFile:
-    filename: str
+    name: str
     pages: Tuple[int, int]
 
-    def get_range(self) -> range:
+    def get_pages_range(self) -> range:
         return range(self.pages[0], self.pages[1] + 1)
 
-def split_pdf(filename: str, splits: List[SplitFile]) -> None:
-    original = PdfFileReader(filename)
+def split_pdf(inputfile: InputFile, splits: List[SplitFile]) -> None:
+    original = PdfFileReader(inputfile.abspath)
     for split in splits:
         output = PdfFileWriter()
-        for page_number in split.get_range():
+        for page_number in split.get_pages_range():
             output.addPage(original.getPage(page_number))
-        with open(f'{split.filename}.pdf', 'wb') as file:
+        with open(f'{inputfile.id}-{split.name}', 'wb') as file:
             output.write(file)
--- pdf-splitter/main.py
+++ pdf-splitter/main.py
@@ -1,21 +1,23 @@
 import argparse
 
-from editor import SplitFile, split_pdf
+from editor import InputFile, SplitFile, split_pdf
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
-    'filename',
+    'filepath',
     type=str,
-    help='name of the input PDF file to split'
+    help="absolute path of the input PDF file to split, or just its name if it's in the same folder"
 )
 parser.add_argument(
     '-s', '--split-name',
     action='append',
+    required=True,
     help="name of a split file, it's not needed to include the extension (.pdf)"
 )
 parser.add_argument(
     '-p', '--page-numbers',
     action='append',
+    required=True,
     type=int,
     nargs=2,
     help='first and last page numbers to split from the input file'
@@ -26,5 +28,6 @@ print(args)
 if len(args.page_numbers) != len(args.split_name):
     raise argparse.ArgumentTypeError('specify the same number of page ranges as the number of splits.')
 
-splits = [SplitFile(name, pages) for name, pages in zip(args.split_name, args.page_numbers)]
-split_pdf(args.filename, splits)
+inputfile = InputFile(args.filepath)
+splits = [SplitFile(f'{name}.pdf', pages) for name, pages in zip(args.split_name, args.page_numbers)]
+split_pdf(inputfile, splits)

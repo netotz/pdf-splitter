57d971884f13d9ba235b657c6533ceafdc5bdb63
netotz <neto.otz@hotmail.com>
2021-07-07 10:27:05 -0500

    Use parsed arguments from console to split PDF

--- pdf-splitter/main.py
+++ pdf-splitter/main.py
@@ -1,5 +1,7 @@
 import argparse
 
+from editor import SplitFile, split_pdf
+
 parser = argparse.ArgumentParser()
 parser.add_argument(
     'filename',
@@ -14,7 +16,15 @@ parser.add_argument(
 parser.add_argument(
     '-p', '--page-numbers',
     action='append',
+    type=int,
     nargs=2,
     help='first and last page numbers to split from the input file'
 )
+
 args = parser.parse_args()
+print(args)
+if len(args.page_numbers) != len(args.split_name):
+    raise argparse.ArgumentTypeError('specify the same number of page ranges as the number of splits.')
+
+splits = [SplitFile(name, pages) for name, pages in zip(args.split_name, args.page_numbers)]
+split_pdf(args.filename, splits)
